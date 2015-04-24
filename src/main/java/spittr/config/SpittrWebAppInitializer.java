aa76bf466921ef24e8e6d803586322b5a2188f82
Hind Github <hindgithub@gmail.com>
2015-04-25 00:25:46 +0200

    134

--- src/main/java/spittr/config/SpittrWebAppInitializer.java
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -1,5 +1,25 @@
 package spittr.config;
 
-public class SpittrWebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
