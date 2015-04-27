56c3343c9e9ef53affdd2fe9fd03fd0e47247b8f
Hind Github <hindgithub@gmail.com>
2015-04-28 00:48:39 +0200

    138

--- src/main/java/spittr/config/SpittrWebAppInitializer.java
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -11,7 +11,7 @@ public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherS
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[]{};
+		return new Class<?>[]{WebConfig.class};
 	}
 
 	@Override
