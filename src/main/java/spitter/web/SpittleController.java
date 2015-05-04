41de1e6e8b57f1ae8c54730d07e39a7890d5d015
Hind Github <hindgithub@gmail.com>
2015-05-04 23:01:01 +0200

    148

--- src/main/java/spitter/web/SpittleController.java
+++ src/main/java/spitter/web/SpittleController.java
@@ -1,11 +1,16 @@
 package spitter.web;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import spitter.data.SpittleRepository;
 
 @Controller
+@RequestMapping("/spittles")
 public class SpittleController {
 
 	private SpittleRepository spittleRepository;
@@ -15,4 +20,9 @@ public class SpittleController {
 		this.spittleRepository = spittleRepository;
 	}
 
+	@RequestMapping(method = GET)
+	public String spittles(Model model) {
+		model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+		return "spittles";
+	}
 }
